fbb4ae92f1ff1df2cc2d1172764ca7ab1b9b7f9d
WoodieTM <woodtatz@gmail.com>
2017-03-20 18:28:09 -0600

    blah

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavLink from './NavLink';
 import { inject, observer } from 'mobx-react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, NavItem, NavbarBrand, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavbarBrand, NavDropdown, MenuItem, Checkbox } from 'react-bootstrap';
 
 class ControlBar extends React.Component {
   render() {
@@ -14,12 +14,9 @@ class ControlBar extends React.Component {
           <Navbar>
             <Navbar .Header><Navbar .Brand>Welcome, {this.props.userStore.name}</Navbar .Brand></Navbar .Header>
             <Nav pullRight>
-              <NavDropdown eventKey={2} title="Collections" id="basic-nav-dropdown">
-                <MenuItem eventKey={2.1}>States</MenuItem>
-                <MenuItem eventKey={2.2}>National Parks</MenuItem>
-                <MenuItem eventKey={2.3}>5-Star Restaurants</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={2.4}>Starbucks</MenuItem>
+              <NavDropdown title="Collections" id="basic-nav-dropdown">
+                <LinkContainer to={{pathname: "/"}}><MenuItem>States</MenuItem></LinkContainer>
+                <MenuItem>National Parks</MenuItem>
               </NavDropdown>
               <LinkContainer to={{pathname: "/"}}><NavItem eventKey={3}>LogOut</NavItem></LinkContainer>
             </Nav>
