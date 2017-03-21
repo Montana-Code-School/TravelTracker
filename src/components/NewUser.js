e985194b87e8794b71ba188c043c80eea5ba80cd
WoodieTM <woodtatz@gmail.com>
2017-03-21 13:07:01 -0600

    login and new user page themeing

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavLink from './NavLink';
 import { inject, observer } from 'mobx-react';
 import { Router, Route, browserHistory, IndexRoute, Link } from 'react-router';
-import { Grid, Col, Row, Image, Well } from 'react-bootstrap';
+import { Grid, Col, Row, Image, Well, Form, FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
 
 
 class NewUser extends React.Component {
@@ -57,38 +57,46 @@ class NewUser extends React.Component {
   }
 
   render() {
-    const parentStyle = {background: "url(http://www.nationalgeographic.com/content/dam/travel/rights-exempt/Travel-2016/national-parks-road-trips/sahale-arm-trail-cascades-national-park.jpg) no-repeat center center fixed",  backgroundColor: "transparent", height: "100vh", opacity:"0.8"};
-    const wellStyle = {marginTop: "50%"};
+    const parentStyle = {height:"100vh", width:"100vw", background: "url(http://www.nationalgeographic.com/content/dam/travel/rights-exempt/Travel-2016/national-parks-road-trips/sahale-arm-trail-cascades-national-park.jpg) no-repeat center center fixed"};
+    const wellStyle = {position: "absolute", top: "0", bottom: "0", left: "0", right: "0", margin: "auto", opacity: ".95", fontFamily: "Josefin Sans", backgroundBlendMode: "overlay", height: "350px", width: "500px"};
+    const logoStyle = {position: "absolute", top: "0px", left: "10px", zIndex: "100"};
+    const loginLinkStyle = {float: "right"};
     return (
       <div>
+        <div>
+          <img className="hidden-xs" style={logoStyle} src="https://cdn2.iconfinder.com/data/icons/geest-travel-kit/128/travel_journey-04-2-512.png" width="100" height="100"/>
+        </div>
         <div style={parentStyle}>
-          <Col md={2}/>
-          <Col md={8}>
-          <Well style={wellStyle}>
-          <form method="" role="form">
-              <legend>Please Register</legend>
+          <Well style={wellStyle} bsSize="large">
+            <Form>
+                <legend>"Sign Up For travelTracker"</legend>
 
-              <div className="form-group">
-                <input onChange={this.handleNameChange} value={this.state.name} type="text" className="form-control" id="username" placeholder="username" required="required"/>
-              </div>
+                <FormGroup controlId="formInlineName">
+                  <ControlLabel>Name</ControlLabel>
+                  <FormControl onChange={this.handleNameChange} type="text" placeholder="username" />
+                </FormGroup>
 
-              <div className="form-group">
-                <input onChange={this.handlePasswordChange} value={this.state.password}type="text" className="form-control" id="password" placeholder="password" required="required"/>
-              </div>
+                <FormGroup controlId="formInlinePassword">
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl onChange={this.handlePasswordChange} type="password" placeholder="password" />
+                </FormGroup>
 
-              <div className="form-group">
-                <input onChange={this.handleEmailChange} value={this.state.email}type="text" className="form-control" id="email" placeholder="email"/>
-              </div>
-              <Link to ="/Welcome">Login Page</Link>
-              <button onClick={this.handleNewUser} type="submit" className="btn btn-primary">Submit</button>
-           </form>
+                <FormGroup controlId="formInlineEmail">
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl onChange={this.handleEmailChange} type="text" placeholder="email" />
+                </FormGroup>
+
+                <div style={loginLinkStyle}>
+                  <Link to ="/Welcome" >Login</Link>
+                </div>
+                <Button onClick={this.handleNewUser} type="submit" className="btn btn-primary">Submit</Button>
+            </Form>
           </Well>
-        </Col>
-        <Col md={2}/>
        </div>
+       <style>
+       @import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
+       </style>
       </div>
-
-
     );
   }
 }
