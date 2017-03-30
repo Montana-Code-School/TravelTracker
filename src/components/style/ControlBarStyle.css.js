2679fc4cda8c5b3bb1930e9f8d0aa2a9d768fb1c
Jeffrey Alan Pawlak <jeffrey_pawlak@yahoo.com>
2017-03-30 11:37:53 -0600

    pre modal copy

--- src/components/style/ControlBarStyle.css.js
+++ src/components/style/ControlBarStyle.css.js
@@ -2,13 +2,13 @@ export default {
 
   navbarStyle:{
     zIndex: "1",
-    marginBottom: "50px",
+    marginBottom: "75px",
     height: "50px"
   },
   logoStyle:{
     position: "absolute",
     top: "1.25px",
-    left: "1.25px",
+    left: "10.25px",
     zIndex: "100"
   }
 };
