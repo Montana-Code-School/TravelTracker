b80b2ea9b183063dab1481d4c22118b15ae1391e
WoodieTM <woodtatz@gmail.com>
2017-04-10 15:25:02 -0600

    welcome well sizing

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -70,7 +70,7 @@ class Welcome extends React.Component {
     const wellStyle = {position: "fixed", top: "0px",
       bottom: "0px", left: "0px", right: "0px", margin: "auto",
       opacity: ".95", backgroundBlendMode: "overlay",
-      height: "375px", width: "350px"};
+      height: "400px", width: "350px"};
     const logoStyle = {position: "fixed" , top: "0px",
       left: "10px", zIndex: "100"};
     const newUserLinkStyle = {float: "right"};
